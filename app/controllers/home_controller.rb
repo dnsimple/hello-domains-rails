4b841cfd8bbeb069d5043ed27c22f7f543fbe967
Luca Guidi <me@lucaguidi.com>
2017-01-05 19:27:29 +0100

    Use a custom User Agent for the DNSimple Ruby client

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if signed_in?
-      client = Dnsimple::Client.new(base_url: ENV['DNSIMPLE_BASE_URL'], access_token: current_user.token)
+      client = Dnsimple::Client.new(base_url: ENV['DNSIMPLE_BASE_URL'], user_agent: ENV['DNSIMPLE_USER_AGENT'], access_token: current_user.token)
       @domains = client.domains.list_domains(current_user.uid).data
     end
   end
